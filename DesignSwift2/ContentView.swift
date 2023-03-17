26ecf6843daac6463a55be5c1427b518da347ed8
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 16:35:50 +0100

    Introduction to SwiftUI design 2

--- DesignSwift2/ContentView.swift
+++ DesignSwift2/ContentView.swift
@@ -10,13 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 100.0, height: 100.0)
+                    .foregroundColor(.orange)
+                VStack(alignment: .leading){
+                    Text("Danilo Santana")
+                        .font(.largeTitle)
+                    Text("Brazil")
+                        .font(.title2)
+                        .fontWeight(.ultraLight)
+                    Text ("Racing Driver")
+                        .fontWeight(.regular)
+                    
+                }
+
+            }
+            .padding()
+            Rectangle()
+                .cornerRadius(56)
+                .foregroundColor(.gray)
+                .frame(width: 357.035, height: 600)
+                .padding(0.0)
         }
-        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
