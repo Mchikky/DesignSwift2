918835989c981adb49392bb52084e020565339bd
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 15:26:31 +0100

    Initial Commit

---
+++ DesignSwift2/DesignSwift2App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSwift2App.swift
+//  DesignSwift2
+//
+//  Created by Chikaodili Deng on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignSwift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
